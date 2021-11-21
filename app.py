fb246d9fc32101e250e5d790053c2695fb29f3bc
vcaf <78371221+vcaf@users.noreply.github.com>
2021-11-21 20:24:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,30 +17,30 @@ def index():
 def submit():
     #HTML -> to python file
     if request.method == 'POST':
-        name = request.form['region_name']
+#         name = request.form['region_name']
         year = request.form['year']
-        longitude = request.form['region_longitude']
-        latitude = request.form['region_latitude']
-        cardinal_direction_labeling = request.form['cardinal_direction_labeling']
-        list = [[year, latitude, longitude,name,  cardinal_direction_labeling]]
-        print(list)
-        arr = np.array(list)
-        print(arr)
+#         longitude = request.form['region_longitude']
+#         latitude = request.form['region_latitude']
+#         cardinal_direction_labeling = request.form['cardinal_direction_labeling']
+#         list = [[year, latitude, longitude,name,  cardinal_direction_labeling]]
+#         print(list)
+#         arr = np.array(list)
+#         print(arr)
 
-        result = ValuePredictor(arr)
-        # prediction = str(result)
-#  return render_template('submit.html',prediction=prediction)
+#         result = ValuePredictor(arr)
+#         # prediction = str(result)
+#  return render_template('submit.html',prediction=year)
 
     # Python -> to html file
     return render_template('submit.html', name_pyton = result)
 
-def ValuePredictor(to_predict_list):
-    to_predict = np.array(to_predict_list).reshape(1,5)
-    loaded_model = pickle.load(open('random_forest.pkl','rb'))
-    result = loaded_model.predict(to_predict)
-    print((result[0]))
+# def ValuePredictor(to_predict_list):
+#     to_predict = np.array(to_predict_list).reshape(1,5)
+#     loaded_model = pickle.load(open('random_forest.pkl','rb'))
+#     result = loaded_model.predict(to_predict)
+#     print((result[0]))
 
-    return result[0]
+#     return result[0]
 
 if __name__ == '__main__' :
     app.run(debug=True)
